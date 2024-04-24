522b8ae8b4c14a31720c846d80346efd8f52b140
barroncm <167339025+barroncm@users.noreply.github.com>
2024-04-24 13:38:03 -0500

    Update ShoppingCart.cpp
    adds the beginning of the shopping cart, adds items to the cart and returns the customer name

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -1,1 +1,17 @@
 /* Type your code here */
+#include "ShoppingCart.h"
+#include <iostream>
+#include <algorithm>
+using namespact std;
+
+//defaults from the given program in zybooks
+ShoppingCart::ShoppingCart() : customerName ("none"), currentDate("January 1, 2016") {}
+ShoppingCart::ShoppingCart(string name, string date): customerName(move(name)), currentDate(move(date)){}
+
+//adds the items to the cart
+void Shopping Cart::AddItem(ItemToPurchase item) {
+  cartItems.push_back(move(item));}
+//return the name of the customer of the cart
+string ShoppingCart::GetCustomerName() const {
+  return customerName;}
+//please check to make sure that the variable names line up
