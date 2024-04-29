8476e0070f8dc700b5f2cf1f99223079045f4f12
dj20223 <167351995+dj20223@users.noreply.github.com>
2024-04-28 20:03:32 -0500

    Update ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,1 +1,23 @@
-/* Type your code here */
+#include "ItemToPurchase.h"
+#include <iostream>
+using namespace std;
+ItemToPurchase::ItemToPurchase() : itemName("none"), itemDescription("none"), itemPrice(0), itemQuantity(0) {}
+
+ItemToPurchase::ItemToPurchase(string name, string description, int price, int quantity)
+    : itemName(name), itemDescription(description), itemPrice(price), itemQuantity(quantity) {}
+
+void ItemToPurchase::SetName(string name) {
+    itemName = name;
+}
+
+void ItemToPurchase::SetDescription(string description) {
+    itemDescription = description;
+}
+
+void ItemToPurchase::SetPrice(int price) {
+    itemPrice = price;
+}
+
+void ItemToPurchase::SetQuantity(int quantity) {
+    itemQuantity = quantity;
+}
